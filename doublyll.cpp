99ec22d9f6847a6082f73cb333cbe38e0b71b634
jaiveerSingh-Sankhla <jaiveer.23bcon1277@jecrcu.edu.in>
2026-05-14 23:53:48 +0530

    Create doublyll.cpp

---
+++ doublyll.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+// structure of doubly LL//
+class Node
+{
+    public:
+    int value ;
+    Node* prev;
+    Node* next;
+    
+    Node(int v)
+    {
+        value v;
+        next=NULL;
+        prev=NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
+    
+ 
